00db69618c0171117cacf0ff07b0aaabbc2377c3
adam <adam.watkin@utcleeds.co.uk>
2024-09-09 11:24:47 +0100

    Added a program to simulate a 6 space carpark

---
+++ L3 Readiness/Parking simp/parking.py
@@ -0,0 +1,24 @@
+def addcar(parking, max):
+    pass
+def removecar(parking):
+    pass
+def checkparking(parking):
+    pass
+def closingtime(parking):
+    pass
+
+def main():
+    parking=[]
+    max=6
+
+
+#Welcome to the carpark simulator
+#You have 6 parking spaces in your car park
+#you need to make each subroutine work to add, remove, view (check) and empty out the carpark of 6 spaces
+#build a main menu in main which allows you to access each of the subroutines, passing parameters.
+#make sure to be robust, make sure to use parameter passing and returns (capture them into a variable)
+
+
+
+if __name__ == "__main__":
+    main()
